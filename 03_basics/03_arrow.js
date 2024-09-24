cd858d06866a6104ede21477b3a7df71728ded0e
ashishxsoni <ashishsonich12@gmail.com>
2024-09-24 16:52:17 +0530

    update in 04_iife

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -2,18 +2,19 @@ const user = {
     username: "hitesh",
     price: 999,
 
-    welcomeMessage: function() {
+    welcomeMessage: function () {
+        //this s hm current context o refer karte hai 
         console.log(`${this.username} , welcome to website`);
-        console.log(this);
+        // console.log(this); // isse yh pure o print kr rh hai 
     }
 
 }
 
 // user.welcomeMessage()
-// user.username = "sam"
+// user.username = "sam" //update karne se ab value iske cange ho jayegi
 // user.welcomeMessage()
 
-// console.log(this);
+console.log(this); //
 
 // function chai(){
 //     let username = "hitesh"
@@ -27,10 +28,10 @@ const user = {
 //     console.log(this.username);
 // }
 
-const chai =  () => {
-    let username = "hitesh"
-    console.log(this);
-}
+// const chai = () => {
+//     let username = "hitesh"
+//     console.log(this);
+// }
 
 
 // chai()
@@ -43,10 +44,10 @@ const chai =  () => {
 
 // const addTwo = (num1, num2) => ( num1 + num2 )
 
-const addTwo = (num1, num2) => ({username: "hitesh"})
+// const addTwo = (num1, num2) => ({ username: "hitesh" })
 
 
-console.log(addTwo(3, 4))
+// console.log(addTwo(3, 4))
 
 
 // const myArray = [2, 5, 3, 7, 8]
--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -1,12 +1,37 @@
 // Immediately Invoked Function Expressions (IIFE)
+//normal function
+// function chai() {
+
+//     console.log(`DB CONNECTED`);
+// }
+
+// arroe.js m hmne sikha tha ki ise ham yaha () k andr ek block  jata hai 
+// to yaha basically ho kaya rha h ki hm same function ko call kar rhe hai aur iske just baad ()
+// laga dene se wo yaha immedidaly invoke ho jata hai fn sign aise hai 
+// chai() is type ka hai yaha chai ko (fn sign ) se refer kra hai  yh () ka batata hai immediate invoke
 
 
-(function chai(){
+(function chai() {
     // named IIFE
-    console.log(`DB CONNECTED`);
+    console.log(`DB CONNECTED function`);
+})(); //iife used to make function imediatley invoked 
+//iske last me ";" use kiya h kyunki ek sath do iife function use karne k liye 
+//phle fn k end m just ; hona chahiye 
+
+
+//using arrow
+(() => {
+    console.log('DB CONNECTED Arrow ');
 })();
 
-( (name) => {
+//yaha jaise ek functio bana hua hai chai(argumented) wala 
+//to waise function ka iife bnnae k liye hm aise banante hai 
+// yaha parameter jo hai wo name hai aur us function m jo value pass ho rhi h wo 
+// 'hitesh' hai jaise chai('hitesh") m karte ham 
+
+
+//using arrow
+((name) => {
     console.log(`DB CONNECTED TWO ${name}`);
-} )('hitesh')
+})('hitesh')
 
